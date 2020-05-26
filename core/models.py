504dd72e8695e03c9977f9b7dabc0b939bf0c0e9
Otavio Cury <otaviocury.oc@gmail.com>
2020-05-26 18:26:57 -0300

    Adicionando campos telefones, cpf, rg e email em Usuario

--- core/models.py
+++ core/models.py
@@ -5,6 +5,11 @@ from django.contrib.auth.models import User
 class Usuario(models.Model):
     user = models.OneToOneField(User, on_delete=models.DO_NOTHING)
     nome = models.CharField(max_length=255, null=False)
+    telefone_celular = models.CharField(max_length=255, null=True)
+    telefone_fixo = models.CharField(max_length=255, null=True)
+    cpf = models.CharField(max_length=255, null=True)
+    rg = models.CharField(max_length=255, null=True)
+    email = models.CharField(max_length=255, null=True)
 
     class Meta:
         db_table = 'tbUsuario'
