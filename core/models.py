4ba85e01ad8e855480057bdff736824651758868
Otavio Cury <otaviocury.oc@gmail.com>
2020-05-26 20:02:32 -0300

    Adicionando classe Atendimento e adicionando campos

--- core/models.py
+++ core/models.py
@@ -5,11 +5,11 @@ from django.contrib.auth.models import User
 class Usuario(models.Model):
     user = models.OneToOneField(User, on_delete=models.DO_NOTHING)
     nome = models.CharField(max_length=255, null=False)
-    telefone_celular = models.CharField(max_length=255, null=True)
-    telefone_fixo = models.CharField(max_length=255, null=True)
-    cpf = models.CharField(max_length=255, null=True)
-    rg = models.CharField(max_length=255, null=True)
-    email = models.CharField(max_length=255, null=True)
+    telefone_celular = models.CharField(max_length=255, null=True, default='')
+    telefone_fixo = models.CharField(max_length=255, null=True, default='')
+    cpf = models.CharField(max_length=255, null=True, default='')
+    rg = models.CharField(max_length=255, null=True, default='')
+    email = models.CharField(max_length=255, null=True, default='')
 
     class Meta:
         db_table = 'tbUsuario'
@@ -17,6 +17,22 @@ class Usuario(models.Model):
 
 class Colaborador(Usuario):
     crp = models.CharField(max_length=50, null=False)
+    perfil = models.TextField(default='')
 
     class Meta:
         db_table = 'tbColaborador'
+
+
+class Paciente(Usuario):
+
+    class Meta:
+        db_table = 'tbPaciente'
+
+
+class Atendimento(models.Model):
+    data_atendimento = models.DateField(null=False)
+    colaborador = models.OneToOneField(Colaborador, on_delete=models.DO_NOTHING)
+    paciente = models.OneToOneField(Paciente, on_delete=models.DO_NOTHING)
+
+    class Meta:
+        db_table = 'tbAtendimento'
