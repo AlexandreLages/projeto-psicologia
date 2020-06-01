9ddc18c6447984b729480b08bf3ed1aaa1db4fad
Alexandre Lages <alexandrelaages@gmail.com>
2020-06-01 00:40:41 -0300

    Cadastro de pacient

--- paciente/urls.py
+++ paciente/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.paciente_view, name = 'paciente_view'),
+    path('cadastro/', views.cadastro_paciente_view, name = 'cadastro_paciente_view'),
 ]
--- paciente/views.py
+++ paciente/views.py
@@ -1,8 +1,53 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.shortcuts import redirect
 from django.shortcuts import render
+
+from core.models import Paciente
 
 
 @login_required
 def paciente_view(request):
     if request.method == 'GET':
         return render(request, 'paciente.html')
+
+
+def cadastro_paciente_view(request):
+	if request.method == 'GET':
+		return render(request, 'cadastro_paciente.html')
+	elif request.method == 'POST':
+		usuario = request.POST['usuario']
+		senha = request.POST['senha']
+		confirma_senha = request.POST['confirmarSenha']
+		email = request.POST['email']
+		nome = request.POST['nome']
+		telefone = request.POST['telefone']
+		celular = request.POST['celular']
+		cpf = request.POST['cpf']
+		rg = request.POST['rg']
+		paciente = None
+
+		user = User.objects.filter(username=usuario)
+
+		if not user:
+			user = User.objects.create_user(usuario, email, senha)
+			user.save()
+
+			paciente = Paciente(
+				nome=nome,
+				telefone_celular=celular,
+				telefone_fixo=telefone,
+				cpf=cpf,
+				rg=rg, 
+				email=email
+			)
+
+			user = User.objects.get(username=usuario)
+			paciente.user = user
+			paciente.save()
+		else:
+			return render(request, 'cadastro_paciente.html', {'message': 'Já existe uma pessoa com esse usuário!'})
+
+		return redirect('/user/login/', {'message': 'Cadastro de paciente realizado com sucesso!'})
+
+	return redirect('/user/login/')
